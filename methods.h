f7d0a9624b3de0bcf12955172a52b15e296ada51
nicolacalzone <nicolacalzone14@gmail.com>
2023-05-11 01:37:41 +0200

    1

---
+++ methods.h
@@ -0,0 +1,15 @@
+#ifndef METHODS_H
+#define METHODS_H
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/features2d.hpp>
+
+void BruteForceMethod(cv::Mat img1, cv::Mat img2);
+void SURFmethod(cv::Mat img1, cv::Mat img2);
+void SIFTmethod(cv::Mat img1, cv::Mat img2);
+
+
+#endif
